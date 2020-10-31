0b363cd8f226f3b66cb5b2c46e9f3a03483f8c9c
AlexNikitenko <nikitenko.ae90@gmail.com>
2020-10-31 13:46:22 +0200

    2020-10-31-homework

--- 2020-10-31-homework/javascript/rotate.js
+++ 2020-10-31-homework/javascript/rotate.js
@@ -1,19 +1,20 @@
 let iconSpin = document.querySelector(".icon-spin3");
 let play = document.querySelector(".icon-play");
 let pause = document.querySelector(".icon-pause");
+let hidden = document.querySelector(".hidden");
 
 
 play.addEventListener("click", function(evt) {
     evt.preventDefault();
     iconSpin.classList.add("animate-spin");
-    play.classList.remove("icon-play");
-    play.classList.add("icon-pause");
+    pause.classList.remove("hidden");
+    play.classList.add("hidden");
 });
 
 
 pause.addEventListener("click", function(evt) {
     evt.preventDefault();
     iconSpin.classList.remove("animate-spin");
-    pause.classList.remove("icon-pause");
-    pause.classList.add("icon-play");
+    pause.classList.add("hidden");
+    play.classList.remove("hidden");
 });
