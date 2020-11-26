444398a8c03aec995495a9aa5f14983afd5d5e75
AlexNikitenko <nikitenko.ae90@gmail.com>
2020-11-26 11:26:07 +0200

    2020-11-23-homework

--- 2020-11-23-homework/js/main.js
+++ 2020-11-23-homework/js/main.js
@@ -1,27 +1,27 @@
-// let mainElement = document.querySelector('.main');
-// let width = 500;
-// let height = 300;
-// let color1 = 'yellow';
-// let color2 = 'blue';
-// mainElement.style = `width: ${width}px; height: ${height}px; background: linear-gradient(blue 50%, yellow 50%);`;
+let a = 3;
+let b = -14;
+let c = -5;
 
-let a = document.querySelector('.variable_a');
-let b = document.querySelector('.variable_b');
-let c = document.querySelector('.variable_c');
-let elemD = document.querySelector('.discriminant');
-let buttonResult = document.querySelector('.button');
+let mainDiv = document.querySelector('.main');
 
-// d = b ^ 2 - 4 * a * c;
-a = Number(a);
-b = Number(b);
-c = Number(c);
-let d = b * b - 4 * a * c;
-buttonResult.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    elemD.value = d;
-})
+let d = b * b - (4 * a * c);
 
-
-console.log(b);
-// ax ^ 2 + bx + c = 0
-// d = b ^ 2 - 4 ac
+if (d < 0) {
+    mainDiv.innerHTML = `Квадратное уравнение: ${(a)}x2 + ${(b)}x + ${(c)} = 0;
+    <br>n\ Найдем дискриминант: D = b*b - 4ac = ${b*b-(4*a*c)};
+    <br>D < 0, корней нет`
+} else if (d === 0) {
+    let x = (-b) / 2 * a;
+    mainDiv.innerHTML = `Квадратное уравнение: ${(a)}x2 + ${(b)}x + ${(c)} = 0;
+    <br>Найдем дискриминант: D = b*b - 4ac = ${b*b-(4*a*c)};
+    <br>D = 0, корень один;
+    <br>Корень: x = (-b)/2a = ${-b/(2*a)}`
+} else {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    mainDiv.innerHTML = `Квадратное уравнение: (${a})x2 + (${b})x + (${(c)}) = 0;
+    <br> Найдем дискриминант: D = b*b - 4ac = ${b*b-(4*a*c)};
+    <br> D > 0, корней два;
+    <br> Корень: x1 = (-b + √d)/2a = -(${b}) + √${d})/2${a} = ${x1};
+    <br> x2 = (-b - √d)/2a = -(${b}) - √${d})/2${a} = ${x2}`
+}
