94c11b77e12d84c4b33c2c252e903652ca6726cb
AlexNikitenko <nikitenko.ae90@gmail.com>
2020-11-26 17:12:16 +0200

    2020-11-26-homework

--- 2020-11-26-homework/js/main.js
+++ 2020-11-26-homework/js/main.js
@@ -5,13 +5,11 @@ let str = ''
 for (let i = 2; i <= 14; i++) {
     if (i % 2 === 0) {
         a = a + ' ' + i;
-        console.log(i);
     }
 }
 for (let i = 33; i <= 45; i++) {
     if (i % 3 === 0) {
         b = b + ' ' + i;
-        console.log(i);
     }
 }
 
