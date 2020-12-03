ba8b1b3ace1a8ab3ef6039754672d0744dae9290
AlexNikitenko <nikitenko.ae90@gmail.com>
2020-12-03 15:32:23 +0200

    2020-11-30-homework-corrected

--- 2020-11-30-homework/js/main.js
+++ 2020-11-30-homework/js/main.js
@@ -1,47 +1,106 @@
 // 1) В массиве 9 чисел. Проверить что каждое из чисел в индексах 2, 5, 7 является суммой соседних чисел. Если нет, то заменить на правильное.
+const genArr = () => {
+    return [1, 4, 6, 3, 7, 2, 8, 0, 2];
+}
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-(arr[2] === arr[1] + arr[3]) ? true: arr[2] = arr[1] + arr[3];
-(arr[5] === arr[4] + arr[6]) ? true: arr[5] = arr[4] + arr[6];
-(arr[7] === arr[6] + arr[8]) ? true: arr[7] = arr[6] + arr[8];
+const fixArr = (index, array) => {
+    (arr[index] === arr[index - 1] + arr[index + 1]) ? true: arr[index] = arr[index - 1] + arr[index + 1];
+    return array;
+};
+
+let arr = genArr();
+fixArr(2, arr);
+fixArr(5, arr);
+fixArr(7, arr);
 
 console.log(arr);
 
 // 2) Задать массив со случайными числами. Найти самое меньшее число, его отнять от первого и последнего числа.
 
-let arr1 = [104, 69, 11, 56, 94];
-let min = Math.min.apply(null, arr1);
+const randomInt = (min, max) => {
+    //случайное число от min до (max+1)
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
 
-let result1 = min - arr1[0];
-let result2 = min - arr1[4];
+const genNewArr = (len, min1, max1) => {
+    let arr = [];
+    for (let i = 0; i < len; i++) {
+        arr.push(randomInt(min1, max1));
+    }
+    return arr;
+}
 
-console.log(result1, result2);
+const minValue = (arr) => {
+    let min = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    };
+    return min;
+};
+
+const decrElem = (index, array, diff) => {
+    array[index] = array[index] - diff;
+    console.log('decr:', array);
+}
+
+let arr2 = genNewArr(6, 1, 12);
+console.log('1: ', arr2);
+let min1 = minValue(arr2);
+decrElem(0, arr2, min1);
+let lastEl = arr2.length - 1;
+decrElem(lastEl, arr2, min1);
+console.log('2: ', arr2);
 
 // 3) Задать массив со случайными числами. Каждый элемент массива умножить на 3 и от произведения отнять 5. Записать в новый массив,
 //  вывести в консоль оба массива для сравнения.
 
-let arr2 = [104, 69, 11, 56, 94];
-let arr3 = [];
-
-for (let i = 0; i < arr2.length; i++) {
-    arr3[i] = arr2[i] * 3 - 5;
+const randomInt2 = (min, max) => {
+    //случайное число от min до (max+1)
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
 }
 
-console.log(arr2, arr3);
+const genNewArr2 = (len, min1, max1) => {
+    let arr = [];
+    for (let i = 0; i < len; i++) {
+        arr.push(randomInt2(min1, max1));
+    }
+    return arr;
+}
+
+const fixElem = (array) => {
+    let arr4 = [];
+    for (let i = 0; i < array.length; i++) {
+        arr4[i] = array[i] * 3 - 5;
+    }
+    return arr4;
+};
+
+let arr3 = genNewArr2(6, 1, 12);
+let arr4 = fixElem(arr3);
+
+console.log('1: ', arr3, '2: ', arr4);
 
 // 4) Используя рекурсивные вычисления, найти максимальное число ряда Фибоначчи, которое меньше 9991999, вывести его; вывести число итераций, 
 // необходимое для поиска этого числа.
 
+// let startNum = 9991999;
 let iteration = 0;
-let arr4 = [];
+let arr5 = [];
 
-let fibonachi = (n) => n <= 1 ? n : fibonachi(n - 1) + fibonachi(n - 2);
+const fibonachi = (n) => n <= 1 ? n : fibonachi(n - 1) + fibonachi(n - 2);
 
-for (let i = 0; fibonachi(i) < 9991999; i++) {
-    arr4[i] = fibonachi(i);
-    iteration = iteration + 1;
+const findMaxNum = (startNum) => {
+    let maxNum = 0;
+    for (let i = 0; fibonachi(i) < startNum; i++) {
+        arr5[i] = fibonachi(i);
+        iteration = iteration + 1;
+        maxNum = Math.max.apply(null, arr5);
+    };
+    return `Nums of iterations: ${iteration}, Max Number: ${maxNum}`;
 }
 
-let max = Math.max.apply(null, arr4);
-
-console.log(iteration, max);
+console.log(findMaxNum(9991999));
