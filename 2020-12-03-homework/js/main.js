b7d82eb14fa3dfdfc3c2634ea952a72b928b387e
AlexNikitenko <nikitenko.ae90@gmail.com>
2020-12-03 16:40:24 +0200

    2020-12-03-homework

---
+++ 2020-12-03-homework/js/main.js
@@ -0,0 +1,152 @@
+// 1) Задано 2 массива одинаковой длины n. Получить 3й массив, который есть копия первого, но в индексах 1, n-1,
+//  n/2 значения со второго массива.
+
+let n = 6;
+let arr3 = [];
+
+const randomInt = (min, max) => {
+    //случайное число от min до (max+1)
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+const genNewArr = (n, min1, max1) => {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(randomInt(min1, max1));
+    }
+    return arr;
+}
+
+const fixArr = (arrayNew, arrayFirst, arraySecond, index) => {
+    // let arrayNew = [];
+    for (let i = 0; i < n; i++) {
+        arrayNew[i] = arrayFirst[i];
+    }
+    arrayNew[index] = arraySecond[index];
+
+    return arrayNew;
+};
+
+let arr = genNewArr(n, 1, 10);
+let arr2 = genNewArr(n, 1, 25);
+fixArr(arr3, arr, arr2, 1);
+fixArr(arr3, arr, arr2, n - 1);
+fixArr(arr3, arr, arr2, n / 2);
+
+console.log('1: ', arr, '2: ', arr2, '3: ', arr3);
+
+
+// 2) Задано целое число. Определить количество разрядов. 
+// 3) Задана квадратная матрица. Получить сумму элементов побочной диагонали.
+
+
+
+
+
+// // 1) В массиве 9 чисел. Проверить что каждое из чисел в индексах 2, 5, 7 является суммой соседних чисел. Если нет, то заменить на правильное.
+// const genArr = () => {
+//     return [1, 4, 6, 3, 7, 2, 8, 0, 2];
+// }
+
+// const fixArr = (index, array) => {
+//     (arr[index] === arr[index - 1] + arr[index + 1]) ? true: arr[index] = arr[index - 1] + arr[index + 1];
+//     return array;
+// };
+
+// let arr = genArr();
+// fixArr(2, arr);
+// fixArr(5, arr);
+// fixArr(7, arr);
+
+// console.log(arr);
+
+// // 2) Задать массив со случайными числами. Найти самое меньшее число, его отнять от первого и последнего числа.
+
+// const randomInt = (min, max) => {
+//     //случайное число от min до (max+1)
+//     let rand = min + Math.random() * (max + 1 - min);
+//     return Math.floor(rand);
+// }
+
+// const genNewArr = (len, min1, max1) => {
+//     let arr = [];
+//     for (let i = 0; i < len; i++) {
+//         arr.push(randomInt(min1, max1));
+//     }
+//     return arr;
+// }
+
+// const minValue = (arr) => {
+//     let min = arr[0];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < min) {
+//             min = arr[i];
+//         }
+//     };
+//     return min;
+// };
+
+// const decrElem = (index, array, diff) => {
+//     array[index] = array[index] - diff;
+//     console.log('decr:', array);
+// }
+
+// let arr2 = genNewArr(6, 1, 12);
+// console.log('1: ', arr2);
+// let min1 = minValue(arr2);
+// decrElem(0, arr2, min1);
+// let lastEl = arr2.length - 1;
+// decrElem(lastEl, arr2, min1);
+// console.log('2: ', arr2);
+
+// // 3) Задать массив со случайными числами. Каждый элемент массива умножить на 3 и от произведения отнять 5. Записать в новый массив,
+// //  вывести в консоль оба массива для сравнения.
+
+// const randomInt2 = (min, max) => {
+//     //случайное число от min до (max+1)
+//     let rand = min + Math.random() * (max + 1 - min);
+//     return Math.floor(rand);
+// }
+
+// const genNewArr2 = (len, min1, max1) => {
+//     let arr = [];
+//     for (let i = 0; i < len; i++) {
+//         arr.push(randomInt2(min1, max1));
+//     }
+//     return arr;
+// }
+
+// const fixElem = (array) => {
+//     let arr4 = [];
+//     for (let i = 0; i < array.length; i++) {
+//         arr4[i] = array[i] * 3 - 5;
+//     }
+//     return arr4;
+// };
+
+// let arr3 = genNewArr2(6, 1, 12);
+// let arr4 = fixElem(arr3);
+
+// console.log('1: ', arr3, '2: ', arr4);
+
+// // 4) Используя рекурсивные вычисления, найти максимальное число ряда Фибоначчи, которое меньше 9991999, вывести его; вывести число итераций, 
+// // необходимое для поиска этого числа.
+
+// // let startNum = 9991999;
+// let iteration = 0;
+// let arr5 = [];
+
+// const fibonachi = (n) => n <= 1 ? n : fibonachi(n - 1) + fibonachi(n - 2);
+
+// const findMaxNum = (startNum) => {
+//     let maxNum = 0;
+//     for (let i = 0; fibonachi(i) < startNum; i++) {
+//         arr5[i] = fibonachi(i);
+//         iteration = iteration + 1;
+//         maxNum = Math.max.apply(null, arr5);
+//     };
+//     return `Nums of iterations: ${iteration}, Max Number: ${maxNum}`;
+// }
+
+// console.log(findMaxNum(9991999));
