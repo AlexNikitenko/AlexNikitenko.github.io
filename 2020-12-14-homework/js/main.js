af98892ce625867f885962d268ef93cb0a0c6d46
AlexNikitenko <nikitenko.ae90@gmail.com>
2020-12-14 02:33:51 +0200

    2020-12-14-homework

--- 2020-12-14-homework/js/main.js
+++ 2020-12-14-homework/js/main.js
@@ -1,6 +1,5 @@
 // 1) На странице сделать поле ввода текста(textarea), а также кнопку Go.В поле вводим любой произвольный текст, разделенный пробелами.При нажатии на кнопку Go должно появится необходимое количество полей ввода, при этом каждое поле ввода будет заполнено одним словом из текста.
 // 2) Найти самое длинное слово, вывести его в консоль.
-// 3) С помощью имеющихся слов создать предложение ровно в 30 символов.
 
 const mainText = document.querySelector('.main-textarea');
 const goButton = document.querySelector('.btn');
@@ -8,9 +7,12 @@ const mainEl = document.querySelector('.main');
 
 goButton.addEventListener("click", (event) => {
     let str = mainText.value;
+    let arr = [];
+
     for (let i = 0; i < str.length; i++) {
         if (str[i] === ' ') {
             let oneWord = str.slice(0, i);
+            arr.push(oneWord);
             str = str.slice(i + 1, str.length);
             i = 0;
             let newStr = `<br><textarea class="main-textarea" name="text" id="" cols="30" rows="1">${oneWord}</textarea>`;
@@ -18,5 +20,17 @@ goButton.addEventListener("click", (event) => {
         }
     }
     mainEl.innerHTML = mainEl.innerHTML + `<br><textarea class="main-textarea" name="text" id="" cols="30" rows="1">${str}</textarea>`;
-    return mainEl.innerHTML;
+
+    arr.push(str);
+    let maxLength = arr[0].length;
+    let theLongestWord = '';
+
+    for (j = 1; j < arr.length; j++) {
+        if (arr[j].length > maxLength) {
+            maxLength = arr[j].length;
+            theLongestWord = arr[j];
+        }
+    }
+    console.log("The Longest Word is:", theLongestWord, "Length: ", maxLength);
+    return arr;
 })
