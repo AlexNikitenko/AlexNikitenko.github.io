2362dd9ca17dace93ce37fdb9deb0ab3ea20d111
AlexNikitenko <nikitenko.ae90@gmail.com>
2020-12-27 23:20:22 +0200

    2020-12-21-homework-corrected

--- 2020-12-21-homework/js/main.js
+++ 2020-12-21-homework/js/main.js
@@ -7,8 +7,7 @@ const buttonRight = document.querySelector('.btn-right');
 const buttonLight = document.querySelector('.btn-light');
 const trainLight = document.querySelector('.light-off');
 
-mainEl.style.left = getComputedStyle(mainEl).left;
-
+let clickLeftCount = 0;
 
 const changeColor = () => {
     trainLight.classList.toggle('light-off');
@@ -20,38 +19,80 @@ const changeColor = () => {
     }
 };
 
+const revFunc = () => {
+    mainEl.style.left = getComputedStyle(mainEl).left;
+    mainEl.classList.remove('drive-forward');
+    mainEl.classList.add('drive-reverse');
+    mainEl.style.animationPlayState = 'running';
+
+    let distance = parseInt(getComputedStyle(mainEl).left, 1);
+    let time = `${distance / 315}s`;
+    mainEl.style.animationDuration = time;
+
+};
+
 buttonLight.addEventListener('click', changeColor);
 
 buttonRight.addEventListener('click', () => {
+    // mainEl.style.left = 0;
+
+
     if (mainEl.style.animationPlayState === 'paused' || getComputedStyle(mainEl).animationPlayState === 'paused') {
         mainEl.style.animationPlayState = 'running';
+        // mainEl.style.left = getComputedStyle(mainEl).left
+        mainEl.classList.add('drive-forward');
+        mainEl.classList.remove('drive-reverse');
+
     } else {
         mainEl.style.animationPlayState = 'paused';
     }
-    let distance = 630 - parseInt(getComputedStyle(mainEl).left, 10);
-    let time = `${distance / 126}s`;
+    let distance = 630 - parseInt(getComputedStyle(mainEl).left, 1);
+    let time = `${distance / 315}s`;
     mainEl.style.animationDuration = time;
-    console.log(distance);
-    mainEl.classList.add('drive-forward');
-    mainEl.classList.remove('drive-reverse');
-    mainEl.style.left = getComputedStyle(mainEl).left;
-
 });
+
+// buttonLeft.addEventListener('click', () => {
+//     mainEl.style.left = getComputedStyle(mainEl).left;
+//     mainEl.classList.remove('drive-forward');
+//     mainEl.classList.add('drive-reverse');
+//     // mainEl.style.animationPlayState = 'running';
+//     clickCount = clickCount + 1;
+//     if (clickCount > 1) {
+//         mainEl.style.animationPlayState = 'paused';
+//         clickCount = 0;
+//     }
+
+//     let distance = parseInt(getComputedStyle(mainEl).left, 1);
+//     let time = `${distance / 315}s`;
+//     mainEl.style.animationDuration = time;
+
+// });
 
 buttonLeft.addEventListener('click', () => {
-    if (mainEl.style.animationPlayState === 'paused' || getComputedStyle(mainEl).animationPlayState === 'paused') {
-        mainEl.style.animationPlayState = 'running';
-    } else {
+
+    if (clickLeftCount >= 1) {
         mainEl.style.animationPlayState = 'paused';
+        clickLeftCount = 0;
+
+    } else {
+        mainEl.style.left = getComputedStyle(mainEl).left;
+        mainEl.classList.remove('drive-forward');
+        mainEl.classList.add('drive-reverse');
+        mainEl.style.animationPlayState = 'running';
+        clickLeftCount = clickLeftCount + 1;
+        let distance = parseInt(getComputedStyle(mainEl).left, 1);
+        let time = `${distance / 315}s`;
+        mainEl.style.animationDuration = time;
     }
-    let distance = parseInt(getComputedStyle(mainEl).left, 10);
-    let time = `${distance / 126}s`;
-    mainEl.style.animationDuration = time;
-    mainEl.classList.remove('drive-forward');
-    mainEl.classList.add('drive-reverse');
-    mainEl.style.left = getComputedStyle(mainEl).left;
+
+
 
 });
+
+
+// buttonLeft.addEventListener('click', revFunc);
+
+
 
 
 document.addEventListener('keydown', (event) => {
@@ -61,31 +102,78 @@ document.addEventListener('keydown', (event) => {
             changeColor();
             break;
         case 'ArrowLeft':
-            mainEl.style.left = getComputedStyle(mainEl).left;
-            let distance1 = parseInt(getComputedStyle(mainEl).left, 10);
-            let time1 = `${distance1 / 126}s`;
-            mainEl.style.animationDuration = time1;
-            if (mainEl.style.animationPlayState === 'paused' || getComputedStyle(mainEl).animationPlayState === 'paused') {
-                mainEl.style.animationPlayState = 'running';
-            } else {
-                mainEl.style.animationPlayState = 'paused';
-            }
-            mainEl.classList.remove('drive-forward');
-            mainEl.classList.add('drive-reverse');
+            revFunc();
             break;
         case 'ArrowRight':
-            mainEl.style.left = getComputedStyle(mainEl).left;
             if (mainEl.style.animationPlayState === 'paused' || getComputedStyle(mainEl).animationPlayState === 'paused') {
                 mainEl.style.animationPlayState = 'running';
+
             } else {
                 mainEl.style.animationPlayState = 'paused';
             }
-            let distance2 = 630 - parseInt(getComputedStyle(mainEl).left, 10);
-            let time2 = `${distance2 / 126}s`;
-            mainEl.style.animationDuration = time2;
-            console.log(distance2);
             mainEl.classList.add('drive-forward');
             mainEl.classList.remove('drive-reverse');
+            let distance = 630 - parseInt(getComputedStyle(mainEl).left, 1);
+            let time = `${distance / 315}s`;
+            mainEl.style.animationDuration = time;
             break;
     }
 });
+
+// const side = 5;
+// // const arr = [];
+
+
+const genArr = (side) => {
+    let arr = [];
+    for (let i = 0; i < side; i++) {
+        let arrIn = [];
+        for (let j = 0; j < side; j++) {
+            arrIn[j] = 0;
+            arr[i] = arrIn;
+        }
+    }
+    return arr;
+};
+
+let arr1 = genArr(5);
+
+const clearArr = (array) => {
+    array.forEach((item, index, array) => {
+        for (let i = 0; i < array.length; i++) {
+            item[index] = null;
+            array[i] = item;
+        }
+    })
+    return array;
+};
+
+console.log(clearArr(arr1));
+
+let arr2 = [
+    [12, 3, 41],
+    [1, null, 3],
+    [0, null, 2]
+];
+
+
+const countFilledCells = (array) => {
+    let count = 0;
+    array.forEach((item, index, array) => {
+        for (let i = 0; i < array.length; i++) {
+            item[i] ? count = (count + 1) : false;
+        }
+    })
+    return count;
+}
+
+// const countFilledCells = (array) => {
+//     let count = 0;
+//     array.forEach((item, index) => item[index] ? count = (count + 1) : false);
+//     // return count;
+//     console.log(array.forEach((item, index) => item[index] ? count = (count + 1) : false))
+// }
+
+
+
+console.log(countFilledCells(arr2));
