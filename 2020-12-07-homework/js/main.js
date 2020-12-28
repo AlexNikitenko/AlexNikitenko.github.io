41c5d46e40ea3f87c374dde90754eecf6e8dae9c
AlexNikitenko <nikitenko.ae90@gmail.com>
2020-12-28 11:34:20 +0200

    2012-12-07-homework-done

--- 2020-12-07-homework/js/main.js
+++ 2020-12-07-homework/js/main.js
@@ -3,27 +3,36 @@
 //   Задача: описать в объекте все возможные конфигурации автомобилей, при этом на каждой конфигурации указать код формата: XXXXYZWV, 
 //   где: XXXX- порядковый номер конфигурации, Y- цвет кузова, Z-коробка передач, W- кондиционер, V-отделка салона.
 
-const obj = {
-    cabin_color: {
-        Y: "yellow",
-        G: "green",
-        R: "red",
-        B: "blue",
-        O: "orange",
-    },
-    transmition: {
-        A: "automatic",
-        M: "manual",
-    },
-    conditioneer: {
-        W: "with",
-        N: "without",
-    },
-    saloon_material: {
-        L: "leather",
-        T: "textile",
-        C: "mixed",
-    },
+const mainObj = {
+    arr: [],
+};
+
+const cabinColor = ['yellow', 'green', 'red', 'blue', 'orange'];
+const transmitionType = ['automatic', 'manual'];
+const conditioneer = ['with', 'without'];
+const saloonMaterial = ['leather', 'textile', 'mixed'];
+
+function Item(y, z, w, v) {
+    this.cabinColor = y;
+    this.transmitionType = z;
+    this.conditioneer = w;
+    this.saloonMaterial = v;
 }
 
-console.log(obj.transmition.M);
+let count = 1;
+
+cabinColor.forEach(cabcol => {
+    transmitionType.forEach(trans => {
+        conditioneer.forEach(cond => {
+            saloonMaterial.forEach(sal => {
+                let tempObj = new Item(cabcol, trans, cond, sal);
+                let tempStr = `${String(count).padStart(4, '0')}${(cabcol[0]).toUpperCase()}${(trans[0]).toUpperCase()}${(cond[0]).toUpperCase()}${(sal[0]).toUpperCase()}`;
+                tempObj.code = tempStr;
+                mainObj.arr.push(tempObj);
+                count++;
+            })
+        })
+    })
+})
+
+console.dir(mainObj);
