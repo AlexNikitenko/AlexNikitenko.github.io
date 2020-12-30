dc7807a0fa8faa0bd0b53a5fd07e8031f3c3ae69
AlexNikitenko <nikitenko.ae90@gmail.com>
2020-12-30 11:00:40 +0200

    2020-12-26-homework

--- 2020-12-26-homework/js/main.js
+++ 2020-12-26-homework/js/main.js
@@ -7,20 +7,41 @@ const mainTimer = document.querySelector('.main-timer');
 btnPlay.disabled = true;
 
 let timeVal;
+let checkTime;
 
 const runTimer = () => {
 
     console.log(Date.now());
     let currTime = moment();
     let destTime = moment(`${timeVal}`, 'HH:mm');
+    let diffTime = moment(destTime.diff(currTime)).utc();
+    checkTime = diffTime;
+
     if (moment(destTime).isBefore(currTime)) {
         destTime.add(1, 'd');
     }
-    let diffTime = moment(destTime.diff(currTime));
-    diffTime.subtract(3, 'h');
-    console.log('difftime:', moment(diffTime).format('HH:mm:ss'));
-    mainTimer.innerHTML = moment(diffTime).format('HH:mm:ss')
+
+    if (diffTime.isDST()) {
+        diffTime.subtract(3, 'h');
+    }
+    console.log('difftime:', diffTime.format('HH:mm:ss'));
+    mainTimer.innerHTML = diffTime.format('HH:mm:ss');
 };
+
+let timerID;
+let delay = 1000;
+
+const timerStart = () => {
+    timerID = setInterval(runTimer, 1000);
+}
+
+const timerStop = () => {
+    clearInterval(timerID);
+    mainTimer.innerHTML = '00:00:00';
+    iconSpin.classList.remove("animate-spin");
+    btnPause.classList.add("hidden");
+    btnPlay.classList.remove("hidden");
+}
 
 btnPlay.addEventListener("click", (evt) => {
     iconSpin.classList.add("animate-spin");
@@ -32,15 +53,16 @@ btnPause.addEventListener("click", (evt) => {
     iconSpin.classList.remove("animate-spin");
     btnPause.classList.add("hidden");
     btnPlay.classList.remove("hidden");
+    timerStop();
 });
 
 inputTime.addEventListener('input', (evt) => {
     if (evt.target.value !== "") {
         btnPlay.disabled = false;
-        btnPlay.addEventListener('click', runTimer);
+        btnPlay.addEventListener('click', timerStart);
     } else {
         btnPlay.disabled = true;
-        btnPlay.removeEventListener('click', runTimer);
+        btnPlay.removeEventListener('click', timerStart);
     }
     timeVal = evt.target.value;
 })
