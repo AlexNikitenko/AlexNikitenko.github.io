cfea2b15317b4794dc06db80a1b3b3512449a376
AlexNikitenko <nikitenko.ae90@gmail.com>
2021-01-02 20:48:41 +0200

    2020-12-26-homework-corrected

--- 2020-12-26-homework/js/main.js
+++ 2020-12-26-homework/js/main.js
@@ -1,68 +1,57 @@
 const iconSpin = document.querySelector(".icon-spin3");
 const btnPlay = document.querySelector(".icon-play");
-const btnPause = document.querySelector(".icon-pause");
+const btnStop = document.querySelector(".icon-pause");
 const hidden = document.querySelector(".hidden");
 const inputTime = document.querySelector('.input-time');
 const mainTimer = document.querySelector('.main-timer');
 btnPlay.disabled = true;
 
 let timeVal;
-let checkTime;
 
 const runTimer = () => {
 
-    console.log(Date.now());
-    let currTime = moment();
-    let destTime = moment(`${timeVal}`, 'HH:mm');
-    let diffTime = moment(destTime.diff(currTime)).utc();
-    checkTime = diffTime;
-
-    if (moment(destTime).isBefore(currTime)) {
-        destTime.add(1, 'd');
-    }
-
-    if (diffTime.isDST()) {
-        diffTime.subtract(3, 'h');
-    }
-    console.log('difftime:', diffTime.format('HH:mm:ss'));
-    mainTimer.innerHTML = diffTime.format('HH:mm:ss');
+    const timerID = setInterval(() => {
+        let currTime = moment();
+        let destTime = moment(`${timeVal}`, 'HH:mm');
+        if (moment(destTime).isBefore(currTime)) {
+            destTime.add(1, 'd');
+        }
+        let diffTime = moment(destTime.diff(currTime)).utc();
+        if (diffTime.isDST()) {
+            diffTime.subtract(3, 'h');
+        }
+        moment(diffTime.format('HH:mm:ss')).subtract(100, 'ms');
+        mainTimer.innerHTML = diffTime.format('HH:mm:ss');
+        if (diffTime.format('HH:mm:ss') === '00:00:00') {
+            clearInterval(timerID);
+            iconSpin.classList.remove("animate-spin");
+            btnStop.classList.add("hidden");
+            btnPlay.classList.remove("hidden");
+        }
+    }, 100)
+    btnStop.addEventListener("click", (evt) => {
+        mainTimer.innerHTML = '00:00:00';
+        iconSpin.classList.remove("animate-spin");
+        btnStop.classList.add("hidden");
+        btnPlay.classList.remove("hidden");
+        clearInterval(timerID);
+    });
 };
-
-let timerID;
-let delay = 1000;
-
-const timerStart = () => {
-    timerID = setInterval(runTimer, 1000);
-}
-
-const timerStop = () => {
-    clearInterval(timerID);
-    mainTimer.innerHTML = '00:00:00';
-    iconSpin.classList.remove("animate-spin");
-    btnPause.classList.add("hidden");
-    btnPlay.classList.remove("hidden");
-}
 
 btnPlay.addEventListener("click", (evt) => {
     iconSpin.classList.add("animate-spin");
-    btnPause.classList.remove("hidden");
+    btnStop.classList.remove("hidden");
     btnPlay.classList.add("hidden");
-});
-
-btnPause.addEventListener("click", (evt) => {
-    iconSpin.classList.remove("animate-spin");
-    btnPause.classList.add("hidden");
-    btnPlay.classList.remove("hidden");
-    timerStop();
 });
 
 inputTime.addEventListener('input', (evt) => {
     if (evt.target.value !== "") {
         btnPlay.disabled = false;
-        btnPlay.addEventListener('click', timerStart);
+        btnPlay.addEventListener('click', runTimer);
+
     } else {
         btnPlay.disabled = true;
-        btnPlay.removeEventListener('click', timerStart);
+        btnPlay.removeEventListener('click', runTimer);
     }
     timeVal = evt.target.value;
-})
+});
